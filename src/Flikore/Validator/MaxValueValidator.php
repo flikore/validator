1b850e1b9540333b832558f39d1b63c0a364b16d
George Marques <george@georgemarques.com.br>
2014-03-01 22:37:41 -0300

    Adding Min & Max Value validators

---
+++ src/Flikore/Validator/MaxValueValidator.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Flikore\Validator
+{
+
+    /**
+     * Validates that a number is equal or lesser than a given value.
+     *
+     * @author George Marques <george at georgemarques.com.br>
+     */
+    class MaxValueValidator extends Validator
+    {
+        /**
+         * The maximum valid value.
+         * @var int The maximum valid value.
+         */
+        protected $max;
+
+        /**
+         * The error message for this validator.
+         * @var string The error message for this validator.
+         */
+        protected $message = 'The %key% be equal or lesser than %max%.';
+
+        /**
+         * Creates a new Max Value Validator.
+         * @param int $max The maximum valid value.
+         */
+        public function __construct($max)
+        {
+            if (!is_int($max))
+            {
+                throw new \InvalidArgumentException('The maximum must be a valid integer');
+            }
+
+            $this->max = $max;
+
+            $this->addKeyValue('max', $max);
+        }
+
+        /**
+         * Executes the real validation so it can be reused.
+         * @param mixed $value The value to validate.
+         * @return boolean Whether the value pass the validation.
+         */
+        protected function doValidate($value)
+        {
+            // ignore empty values
+            if(empty($value))
+            {
+                return true;
+            }
+            return ($value <= $this->max);
+        }
+
+    }
+
+}
---
+++ src/Flikore/Validator/MinValueValidator.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Flikore\Validator
+{
+
+    /**
+     * Validates that a number is equal or greater than a given value.
+     *
+     * @author George Marques <george at georgemarques.com.br>
+     */
+    class MinValueValidator extends Validator
+    {
+        /**
+         * The minimum valid value.
+         * @var int The minimum valid value.
+         */
+        protected $min;
+
+        /**
+         * The error message for this validator.
+         * @var string The error message for this validator.
+         */
+        protected $message = 'The %key% be equal or greater than %min%.';
+
+        /**
+         * Creates a new Min Value Validator.
+         * @param int $min The minimum valid value.
+         */
+        public function __construct($min)
+        {
+            if (!is_int($min))
+            {
+                throw new \InvalidArgumentException('The minimum must be a valid integer');
+            }
+
+            $this->min = $min;
+
+            $this->addKeyValue('min', $min);
+        }
+
+        /**
+         * Executes the real validation so it can be reused.
+         * @param mixed $value The value to validate.
+         * @return boolean Whether the value pass the validation.
+         */
+        protected function doValidate($value)
+        {
+            // ignore empty values
+            if(empty($value))
+            {
+                return true;
+            }
+            return ($value >= $this->min);
+        }
+
+    }
+
+}
