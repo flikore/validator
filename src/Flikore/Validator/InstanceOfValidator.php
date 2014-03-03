1336c3754da634d7e32b517c83607f54fa08b09f
George Marques <george@georgemarques.com.br>
2014-03-03 01:56:28 -0300

    Adding InstanceOf Validator

---
+++ src/Flikore/Validator/InstanceOfValidator.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Flikore\Validator
+{
+
+    /**
+     * Validates that the value is an instance of a certain class.
+     *
+     * @author George Marques <george at georgemarques.com.br>
+     */
+    class InstanceOfValidator extends Validator
+    {
+
+        /**
+         * The class which the value must be instance of.
+         * @var mixed The class which the value must be instance of.
+         */
+        protected $class;
+
+        /**
+         * The error message for this validator.
+         * @var string The error message for this validator.
+         */
+        protected $message = 'The %key% must be an instance of %class%.';
+
+        /**
+         * Creates a new Instance Of Validator.
+         * @param mixed $class The class which the value must be instance of.
+         */
+        public function __construct($class)
+        {
+            if (!(is_object($class)) && (!class_exists($class)))
+            {
+                throw new \InvalidArgumentException(sprintf('The class "%s" does not exist.', $class));
+            }
+
+            $this->class = $class;
+
+            $this->addKeyValue('class', is_object($class) ? get_class($class) : $class);
+        }
+
+        /**
+         * Executes the real validation so it can be reused.
+         * @param mixed $value The value to validate.
+         * @return boolean Whether the value pass the validation.
+         */
+        protected function doValidate($value)
+        {
+            // ignore empty values
+            if (is_null($value) || $value === '')
+            {
+                return true;
+            }
+            return ($value instanceof $this->class);
+        }
+
+    }
+
+}
