bed4ce3c12ec5dc6abe4998b3b1606d619dbcfb7
George Marques <george@georgemarques.com.br>
2014-03-03 20:13:43 -0300

    Adding Value Between validator

---
+++ src/Flikore/Validator/Validators/ValueBetweenValidator.php
@@ -0,0 +1,99 @@
+<?php
+
+/*
+ * The MIT License
+ *
+ * Copyright 2014 George Marques <george at georgemarques.com.br>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+namespace Flikore\Validator\Validators;
+
+/**
+ * Validates that the value is between a certain range.
+ * 
+ * @customKey <i>%min%</i> The minimum valid value.
+ * @customKey <i>%max%</i> The maximum valid value.
+ *
+ * @author George Marques <george at georgemarques.com.br>
+ * @license http://opensource.org/licenses/MIT MIT
+ * @copyright (c) 2014, George Marques
+ * @package Flikore\Validator
+ */
+class ValueBetweenValidator extends \Flikore\Validator\Validator
+{
+
+    /**
+     * The minimum valid value.
+     * @var int The minimum valid value.
+     */
+    protected $min;
+
+    /**
+     * The maximum valid value.
+     * @var int The maximum valid value.
+     */
+    protected $max;
+
+    /**
+     * The error message for this validator.
+     * @var string The error message for this validator.
+     */
+    protected $message = 'The %key% must be between %min% and %max%.';
+
+    /**
+     * Creates a new Value Between Validator.
+     * @param int $min The minimum valid value.
+     * @param int $max The maximum valid value.
+     */
+    public function __construct($min, $max)
+    {
+        if (!is_int($min))
+        {
+            throw new \InvalidArgumentException('The minimum must be a valid integer');
+        }
+        if (!is_int($max))
+        {
+            throw new \InvalidArgumentException('The maximum must be a valid integer');
+        }
+
+        $this->min = $min;
+        $this->max = $max;
+
+        $this->addKeyValue('min', $min);
+        $this->addKeyValue('max', $max);
+    }
+
+    /**
+     * Executes the real validation so it can be reused.
+     * @param mixed $value The value to validate.
+     * @return boolean Whether the value pass the validation.
+     */
+    protected function doValidate($value)
+    {
+        // ignore empty values
+        if (is_null($value) || $value === '')
+        {
+            return true;
+        }
+        return is_numeric($value) && (($value >= $this->min) && ($value <= $this->max));
+    }
+
+}
---
+++ unittests/src/Flikore/Validator/Validators/ValueBetweenValidatorTest.php
@@ -0,0 +1,116 @@
+<?php
+
+/*
+ * The MIT License
+ *
+ * Copyright 2014 George Marques <george at georgemarques.com.br>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+namespace Flikore\Validator\Validators;
+
+/**
+ * Tests for ValueBetweenValidator class.
+ *
+ * @author George Marques <george at georgemarques.com.br>
+ * @license http://opensource.org/licenses/MIT MIT
+ * @copyright (c) 2014, George Marques
+ * @package Flikore\Validator
+ * @category Tests
+ */
+class ValueBetweenValidatorTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testValidatePass()
+    {
+        $val = new ValueBetweenValidator(2, 4);
+        
+        $this->assertTrue($val->validate(2));
+        $this->assertTrue($val->validate(3));
+        $this->assertTrue($val->validate(4));
+    }
+
+    public function testValidateFail()
+    {
+        $val = new ValueBetweenValidator(2, 4);
+        
+        $this->assertFalse($val->validate(-2));
+        $this->assertFalse($val->validate(0));
+        $this->assertFalse($val->validate(1));
+        $this->assertFalse($val->validate(5));
+        $this->assertFalse($val->validate(PHP_INT_MAX));
+    }
+    
+    public function testValidateNotIntegerSuccess()
+    {
+        $val = new ValueBetweenValidator(2, 4);
+        
+        $this->assertTrue($val->validate('2'));
+        $this->assertTrue($val->validate('3'));
+        $this->assertTrue($val->validate('4'));
+    }
+    
+    public function testValidateNotIntegerFail()
+    {
+        $val = new ValueBetweenValidator(2, 4);
+        
+        $this->assertFalse($val->validate('not a number'));
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testWrongMinValueArgument()
+    {
+        $t = new ValueBetweenValidator('aa', 4);
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testWrongMaxValueArgument()
+    {
+        $t = new ValueBetweenValidator(4, 'a');
+    }
+
+    public function testValidateEmptyValue()
+    {
+        $val = new ValueBetweenValidator(2, 4);
+        $this->assertTrue($val->validate(''));
+        $this->assertTrue($val->validate(null));
+    }
+
+    public function testMinValueMessage()
+    {
+        $val = new ValueBetweenValidator(0, 3);
+        $val->setErrorMessage('%min%');
+        
+        $this->assertEquals('0', $val->getErrorMessage());
+    }
+    
+    public function testMaxValueMessage()
+    {
+        $val = new ValueBetweenValidator(0, 3);
+        $val->setErrorMessage('%max%');
+        
+        $this->assertEquals('3', $val->getErrorMessage());
+    }
+
+}
