b2c6363b49971d89c3869dae07f66b77ab4a68a6
George Marques <george@georgemarques.com.br>
2014-03-04 17:40:51 -0300

    Adding Equals validator

---
+++ src/Flikore/Validator/Validators/EqualsValidator.php
@@ -0,0 +1,93 @@
+<?php
+
+/*
+ * The MIT License
+ *
+ * Copyright 2014 George Marques <george at georgemarques.com.br>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+namespace Flikore\Validator\Validators;
+
+/**
+ * Validates if a value is equal to another.
+ * 
+ * @customKey <i>%compare%</i> The value to be compared to.
+ * @customKey <i>%strict%</i> Whether the comparison is done in strict form.
+ *
+ * @author George Marques <george at georgemarques.com.br>
+ * @license http://opensource.org/licenses/MIT MIT
+ * @copyright (c) 2014, George Marques
+ * @package Flikore\Validator
+ */
+class EqualsValidator extends \Flikore\Validator\Validator
+{
+
+    /**
+     * The error message for this validator.
+     * @var string The error message for this validator.
+     */
+    protected $message = 'The %key% must be equal to %compare%.';
+
+    /**
+     * The value to be compared to.
+     * @var mixed The value to be compared to.
+     */
+    protected $compare;
+
+    /**
+     * Whether the comparison should be done in strict form.
+     * @var boolean Whether the comparison should be done in strict form.
+     */
+    protected $strict;
+
+    /**
+     * Creates a new Equals Validator.
+     * @param mixed $compare The value to compare to.
+     * @param boolean $strict Whether the comparison should be strict.
+     */
+    public function __construct($compare, $strict = false)
+    {
+        $this->compare = $compare;
+        $this->strict = (bool) $strict;
+
+        $this->addKeyValue('compare', $compare);
+        $this->addKeyValue('strict', $strict ? 'true' : 'false');
+    }
+
+    /**
+     * Executes the real validation so it can be reused.
+     * @param mixed $value The value to validate.
+     * @return boolean Whether the value pass the validation.
+     */
+    protected function doValidate($value)
+    {
+        // ignore empty values
+        if (is_null($value) || $value === '')
+        {
+            return true;
+        }
+        $t1 = gettype($value);
+        $t2 = gettype($this->compare);
+        $comp = $t1 == $t2;
+        return $this->strict ? ($value === $this->compare) : ($value == $this->compare);
+    }
+
+}
---
+++ unittests/src/Flikore/Validator/Validators/EqualsValidatorTest.php
@@ -0,0 +1,120 @@
+<?php
+
+/*
+ * The MIT License
+ *
+ * Copyright 2014 George Marques <george at georgemarques.com.br>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+namespace Flikore\Validator\Validators;
+
+/**
+ * Tests for EqualsValidator class.
+ *
+ * @author George Marques <george at georgemarques.com.br>
+ * @license http://opensource.org/licenses/MIT MIT
+ * @copyright (c) 2014, George Marques
+ * @package Flikore\Validator
+ * @category Tests
+ */
+class EqualsValidatorTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testValidatePass()
+    {
+        $v = new EqualsValidator('aa');
+        $this->assertTrue($v->validate('aa'));
+        $this->assertTrue($v->validate(0));
+        
+        $v = new EqualsValidator(0);
+        $this->assertTrue($v->validate(0));
+        $this->assertTrue($v->validate('0'));
+        $this->assertTrue($v->validate('a'));
+        
+        $v = new EqualsValidator(new \stdClass());
+        $this->assertTrue($v->validate(new \stdClass()));
+    }
+
+    public function testValidateFail()
+    {
+        $v = new EqualsValidator('aa');
+        $this->assertFalse($v->validate('aaa'));
+        
+        $v = new EqualsValidator(0);
+        $this->assertFalse($v->validate(1));
+        $this->assertFalse($v->validate('1'));
+        
+        $v = new EqualsValidator(new \stdClass());
+        $this->assertFalse($v->validate(new \ArrayIterator()));
+    }
+    
+    public function testValidateStrictPass()
+    {
+        $v = new EqualsValidator('aa', true);
+        $this->assertTrue($v->validate('aa'));
+        
+        $v = new EqualsValidator(0, true);
+        $this->assertTrue($v->validate(0));
+        
+        $obj = new \stdClass();
+        $v = new EqualsValidator($obj, true);
+        $this->assertTrue($v->validate($obj));
+    }
+
+    public function testValidateStrictFail()
+    {
+        $v = new EqualsValidator('aa', true);
+        $this->assertFalse($v->validate('aaa'));
+        
+        $v = new EqualsValidator(0, true);
+        $this->assertFalse($v->validate(1));
+        $this->assertFalse($v->validate('0'));
+        
+        $v = new EqualsValidator(new \stdClass(), true);
+        $this->assertFalse($v->validate(new \stdClass()));
+    }
+
+    public function testValidateEmptyValue()
+    {
+        $v = new EqualsValidator('any');
+
+        $this->assertTrue($v->validate(''));
+        $this->assertTrue($v->validate(null));
+    }
+    
+    public function testMessageKeys()
+    {
+        $v = new EqualsValidator('any', true);
+        
+        $v->setErrorMessage('%compare%');
+        $this->assertEquals('any', $v->getErrorMessage());
+        
+        $v->setErrorMessage('%strict%');
+        $this->assertEquals('true', $v->getErrorMessage());
+        
+        $v = new EqualsValidator('any', false);
+                
+        $v->setErrorMessage('%strict%');
+        $this->assertEquals('false', $v->getErrorMessage());
+        
+    }
+
+}
