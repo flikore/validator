d96379329df435ec53560151cc6798b6912d5b31
George Marques <george@georgemarques.com.br>
2014-03-07 12:55:45 -0300

    Adding Less Than validator

---
+++ src/Flikore/Validator/Validators/LessThanValidator.php
@@ -0,0 +1,85 @@
+<?php
+
+/*
+ * The MIT License
+ *
+ * Copyright 2014 George Marques <george at georgemarques.com.br>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+namespace Flikore\Validator\Validators;
+
+/**
+ * Validates that a number is lesser than a given value.
+ * 
+ * @customKey <i>%max%</i> The exclusive upper boundary.
+ *
+ * @author George Marques <george at georgemarques.com.br>
+ * @license http://opensource.org/licenses/MIT MIT
+ * @copyright (c) 2014, George Marques
+ * @package Flikore\Validator
+ */
+class LessThanValidator extends \Flikore\Validator\Validator
+{
+
+    /**
+     * The exclusive upper boundary.
+     * @var int The exclusive upper boundary.
+     */
+    protected $max;
+
+    /**
+     * The error message for this validator.
+     * @var string The error message for this validator.
+     */
+    protected $message = 'The %key% must be lesser than %max%.';
+
+    /**
+     * Creates a new Less Than Validator.
+     * @param int $max The exclusive upper boundary.
+     */
+    public function __construct($max)
+    {
+        if (!is_int($max))
+        {
+            throw new \InvalidArgumentException(dgettext('Flikore.Validator','The limit must be a valid integer'));
+        }
+
+        $this->max = $max;
+
+        $this->addKeyValue('max', $max);
+    }
+
+    /**
+     * Executes the real validation so it can be reused.
+     * @param mixed $value The value to validate.
+     * @return boolean Whether the value pass the validation.
+     */
+    protected function doValidate($value)
+    {
+        // ignore empty values
+        if (is_null($value) || $value === '')
+        {
+            return true;
+        }
+        return ($value < $this->max);
+    }
+
+}
