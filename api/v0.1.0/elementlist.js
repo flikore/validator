629e3fe72559b8f1d00b2daac7d8f4b43dff4138
George Marques <george@georgemarques.com.br>
2014-03-17 00:49:16 -0300

    Updating API files

--- api/v0.1.0/elementlist.js
+++ api/v0.1.0/elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","Exception"],["c","Flikore\\Validator\\ExactLengthValidator"],["c","Flikore\\Validator\\Exception\\ValidatorException"],["c","Flikore\\Validator\\LengthBetweenValidator"],["c","Flikore\\Validator\\MaxLengthValidator"],["c","Flikore\\Validator\\MinLengthValidator"],["c","Flikore\\Validator\\NotEmptyValidator"],["c","Flikore\\Validator\\ValidationSet"],["c","Flikore\\Validator\\Validator"],["c","OutOfBoundsException"],["c","RuntimeException"]];
+ApiGen.elements = [["c","Flikore\\Validator\\ExactLengthValidator"],["c","Flikore\\Validator\\Exception\\ValidatorException"],["c","Flikore\\Validator\\LengthBetweenValidator"],["c","Flikore\\Validator\\MaxLengthValidator"],["c","Flikore\\Validator\\MinLengthValidator"],["c","Flikore\\Validator\\NotEmptyValidator"],["c","Flikore\\Validator\\ValidationSet"],["c","Flikore\\Validator\\Validator"]];
